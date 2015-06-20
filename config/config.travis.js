1e5d6f33c32478b346bfed97f85d9ea6cf320f52
Ben Booth <bkbooth@gmail.com>
2015-06-20 15:53:53 +1000

    Add Facebook auth config for Travis CI

--- config/config.travis.js
+++ config/config.travis.js
@@ -1,6 +1,14 @@
 // Travis CI environment config
 module.exports = {
+  secret: 'thisisnotsecretbutitsjustfortravis',
   db: {
     url: 'mongodb://localhost/liow2'
+  },
+  auth: {
+    facebook: {
+      clientID: '1438693386453447',
+      clientSecret: 'f0380da09ad0aebd63a3003f09553fbe',
+      callbackURL: 'http://loveisourweapon.travis:3000/auth/facebook/callback'
+    }
   }
 };
