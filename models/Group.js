9ea31ad8ecd4aa1aa127968e125649f1d1d98518
Ben Booth <bkbooth@gmail.com>
2015-09-01 16:50:29 +1000

    Add tests for an improve Group model

--- models/Group.js
+++ models/Group.js
@@ -2,11 +2,30 @@ var _ = require('lodash'),
     mongoose = require('mongoose'),
     ObjectId = mongoose.Schema.Types.ObjectId;
 
+// At least one admin
+function validateHasAdmin(admins) {
+  return admins.length > 0;
+}
+
+// One of the admins is the owner
+function validateOwnerIsAdmin(admins) {
+  return !! _.find(admins, function __find(admin) {
+    return admin === this.owner;
+  }.bind(this));
+}
+
 var GroupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   url_name: { type: String, required: true, index: { unique: true } },
   owner: { type: ObjectId, ref: 'User', required: true },
-  admins: [{ type: ObjectId, ref: 'User', required: true }],
+  admins: {
+    type: [{ type: ObjectId, ref: 'User' }],
+    required: true,
+    validate: [
+      { validator: validateHasAdmin, msg: 'An admin user is required', type: 'hasadmin' },
+      { validator: validateOwnerIsAdmin, msg: 'The owner needs to be an admin', type: 'ownerisadmin' }
+    ]
+  },
   country: { type: ObjectId, ref: 'Country' },
   logo: String,
   cover_image: String,
---
+++ test/models/Group.js
@@ -0,0 +1,90 @@
+var utils = require('../../utils/tests'),
+    expect = require('chai').expect,
+    _ = require('lodash');
+
+var ObjectId = require('mongoose').Types.ObjectId,
+    Group = require('../../models/Group');
+
+var groupOwner = ObjectId();
+var validGroup = {
+  name: 'Group Name',
+  owner: groupOwner,
+  admins: [groupOwner]
+};
+
+describe('Group', function __describe() {
+  before(utils.dbConnect);
+  after(utils.dbDisconnect);
+
+  describe('#save()', function __describe() {
+    afterEach(function __afterEach(done) {
+      Group.remove({}, function __groupRemove(err) {
+        if (err) { return done(err); }
+
+        done();
+      });
+    }); // afterEach()
+
+    it('should require name, url_name, owner and admins', function __it(done) {
+      var group = new Group();
+
+      group.save(function __groupSave(err, group) {
+        expect(err).to.exist.and.to.have.property('name', 'ValidationError');
+        expect(err).to.have.deep.property('errors.name.kind', 'required');
+        expect(err).to.have.deep.property('errors.url_name.kind', 'required');
+        expect(err).to.have.deep.property('errors.owner.kind', 'required');
+        expect(err).to.have.deep.property('errors.admins.kind', 'required');
+        expect(group).to.not.exist;
+
+        done();
+      });
+    }); // it()
+
+    it('should require owner to be an admin', function __it(done) {
+      var group = new Group({
+        name: 'Group Name',
+        owner: ObjectId(),
+        admins: [ObjectId()]
+      });
+
+      group.save(function __groupSave(err, group) {
+        expect(err).to.have.deep.property('errors.admins.kind', 'ownerisadmin');
+        expect(group).to.not.exist;
+
+        done();
+      });
+    }); // it()
+
+    it('should create url_name as a kebab case copy of name', function __it(done) {
+      var group = new Group(validGroup);
+
+      group.save(function __groupSave(err, group) {
+        expect(err).to.not.exist;
+        expect(group).to.have.property('url_name', _.kebabCase(validGroup.name));
+
+        done();
+      });
+    }); // it()
+
+    it('should save a valid Group', function __it(done) {
+      var group = new Group(validGroup);
+
+      group.save(function __groupSave(err, group) {
+        expect(err).to.not.exist;
+        expect(group).to.be.an('object').and.an.instanceof(Group);
+
+        done();
+      });
+    }); // it()
+  }); // describe()
+
+  describe('#getFilter()', function __describe() {
+    it('should return an array of strings', function __it(done) {
+      var filter = Group.getFilter();
+
+      expect(filter).to.be.an('array').and.have.length.above(0);
+
+      done();
+    }); // it()
+  }); // describe()
+}); // describe()
