1fdc1a88093e430742d34c863be3fa3a33cb351f
Ben Booth <bkbooth@gmail.com>
2015-09-03 10:51:21 +1000

    Add tests for and improve /acts routes

--- routes/acts.js
+++ routes/acts.js
@@ -59,12 +59,16 @@ router.post('/', function __postAct(req, res, next) {
   req.body.group = ObjectId.isValid(req.body.group) ? ObjectId(req.body.group) : null;
   req.body.deed = ObjectId.isValid(req.body.deed) ? ObjectId(req.body.deed) : null;
 
-  var act = new Act(req.body);
-  act.save(function __actSave(err) {
+  new Act(req.body).save(function __actSave(err, act) {
     if (err) { return next(err); }
 
-    res.status(201).send();
+    res.status(201).location('/acts/' + act._id).json(act);
   });
+});
+
+/* GET /acts/:act_id */
+router.get('/:act_id', function __getAct(req, res) {
+  res.status(200).json(req.act);
 });
 
 /* DELETE /acts/:act_id */
@@ -78,8 +82,8 @@ router.delete('/:act_id', function __deleteAct(req, res, next) {
 
 /* GET /acts/:act_id/likes */
 router.get('/:act_id/likes', function __getActLikes(req, res, next) {
-  Like.find({ 'target.act': req.act }, function __likeFindByAct(err, likes) {
-    if (err){ return next(err); }
+  Like.find({ 'target.act': req.act._id }, function __likeFindByAct(err, likes) {
+    if (err) { return next(err); }
 
     res.status(200).json(likes);
   });
@@ -89,18 +93,17 @@ router.get('/:act_id/likes', function __getActLikes(req, res, next) {
 router.post('/:act_id/likes', function __postActLike(req, res, next) {
   req.body = _.pick(req.body, Like.getFilter());
   req.body.user = ObjectId.isValid(req.body.user) ? ObjectId(req.body.user) : null;
-  req.body.target = { act: req.act };
+  req.body.target = { act: req.act._id };
 
-  var like = new Like(req.body);
-  like.save(function __likeSave(err) {
+  new Like(req.body).save(function __likeSave(err, like) {
     if (err) { return next(err); }
 
-    res.status(201).send();
+    res.status(201).location('/acts/' + req.act._id + '/likes/' + like._id).json(like);
   });
 });
 
 /* DELETE /acts:act_id/likes/:like_id */
-router.delete(':act_id/likes/:like_id', function __deleteActLike(req, res, next) {
+router.delete('/:act_id/likes/:like_id', function __deleteActLike(req, res, next) {
   req.like.remove(function __likeRemove(err) {
     if (err) { return next(err); }
 
@@ -110,7 +113,7 @@ router.delete(':act_id/likes/:like_id', function __deleteActLike(req, res, next)
 
 /* GET /acts/:act_id/comments */
 router.get('/:act_id/comments', function __getActComments(req, res, next) {
-  Comment.find({ 'target.act': req.act }, function __commentFindByAct(err, comments) {
+  Comment.find({ 'target.act': req.act._id }, function __commentFindByAct(err, comments) {
     if (err) { return next(err); }
 
     res.status(200).json(comments);
@@ -121,13 +124,12 @@ router.get('/:act_id/comments', function __getActComments(req, res, next) {
 router.post('/:act_id/comments', function __postActComment(req, res, next) {
   req.body = _.pick(req.body, Comment.getFilter());
   req.body.user = ObjectId.isValid(req.body.user) ? ObjectId(req.body.user) : null;
-  req.body.target = { act: req.act };
+  req.body.target = { act: req.act._id };
 
-  var comment = new Comment(req.body);
-  comment.save(function __commentSave(err) {
+  new Comment(req.body).save(function __commentSave(err, comment) {
     if (err) { return next(err); }
 
-    res.status(201).send();
+    res.status(201).location('/acts/' + req.act._id + '/comments/' + comment._id).json(comment);
   });
 });
 
@@ -136,10 +138,10 @@ router.put('/:act_id/comments/:comment_id', function __putActComment(req, res, n
   req.body = _.pick(req.body, Comment.getFilter());
   req.body.modified = new Date();
 
-  req.comment.update(req.body, function __commentUpdate(err) {
+  Comment.findByIdAndUpdate(req.comment._id, req.body, { new: true }, function __commentFindAndUpdate(err, comment) {
     if (err) { return next(err); }
 
-    res.status(204).send();
+    res.status(200).json(comment);
   });
 });
 
---
+++ test/routes/acts.js
@@ -0,0 +1,384 @@
+var utils = require('../../utils/tests'),
+    request = require('supertest'),
+    expect = require('chai').expect,
+    app = require('../../app');
+
+var ObjectId = require('mongoose').Types.ObjectId,
+    Act = require('../../models/Act'),
+    Like = require('../../models/Like'),
+    Comment = require('../../models/Comment');
+
+var actId = null;
+var validAct = {
+  user: ObjectId(),
+  group: ObjectId(),
+  deed: ObjectId()
+};
+
+describe('/acts', function __describe() {
+  before(utils.dbConnect);
+  after(utils.dbDisconnect);
+
+  afterEach(function __afterEach(done) {
+    Act.remove({}, function __actRemove(err) {
+      if (err) { return done(err); }
+
+      done();
+    });
+  }); // afterEach()
+
+  describe('/', function __describe() {
+    it('GET should return status 200 and an empty array', function __it(done) {
+      request(app)
+        .get('/acts')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function __expect(res) {
+          expect(res.body).to.be.an('array').and.to.have.length(0);
+        })
+        .end(done);
+    }); // it()
+
+    it('GET should return status 200 and a non-empty array', function __it(done) {
+      new Act(validAct).save(function __actSave(err) {
+        if (err) { return done(err); }
+
+        request(app)
+          .get('/acts')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function __expect(res) {
+            expect(res.body).to.be.an('array').and.to.have.length.above(0);
+          })
+          .end(done);
+      });
+    }); // it()
+
+    it('POST invalid data should return status 400 and an error message', function __it(done) {
+      request(app)
+        .post('/acts')
+        .send({})
+        .expect(400)
+        .expect('Content-Type', /json/)
+        .expect(function __expect(res) {
+          expect(res.body).property('message', 'Act validation failed');
+        })
+        .end(done);
+    }); // it()
+
+    it('POST valid data should return status 201 and the created Act', function __it(done) {
+      request(app)
+        .post('/acts')
+        .send(validAct)
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .expect('Location', /acts/)
+        .expect(function __expect(res) {
+          expect(res.body).be.be.an('object').and.to.have.property('_id');
+        })
+        .end(done);
+    }); // it()
+  }); // describe()
+
+  describe('/:act_id', function __describe() {
+    beforeEach(function __beforeEach(done) {
+      new Act(validAct).save(function __actSave(err, act) {
+        if (err) { return done(err); }
+
+        actId = act._id;
+
+        done();
+      });
+    }); // beforeEach()
+
+    it('GET invalid ID should return status 400 and an error message', function __it(done) {
+      request(app)
+        .get('/acts/invalid')
+        .expect(400)
+        .expect('Content-Type', /json/)
+        .expect(function __expect(res) {
+          expect(res.body).to.have.property('message', 'Invalid act_id');
+        })
+        .end(done);
+    }); // it()
+
+    it('GET non-existent ID should return status 400 and an error message', function __it(done) {
+      request(app)
+        .get('/acts/' + ObjectId())
+        .expect(400)
+        .expect('Content-Type', /json/)
+        .expect(function __expect(res) {
+          expect(res.body).to.have.property('message');
+        })
+        .end(done);
+    }); // it()
+
+    it('GET valid ID should return status 200 and the Act', function __it(done) {
+      request(app)
+        .get('/acts/' + actId)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function __expect(res) {
+          expect(res.body).to.have.property('_id', actId.toString());
+        })
+        .end(done);
+    }); // it()
+
+    it('DELETE should return status 204 and delete the Act', function __it(done) {
+      request(app)
+        .delete('/acts/' + actId)
+        .expect(204)
+        .expect(function __expect(res) {
+          expect(res.body).to.be.empty;
+        })
+        .end(function __end() {
+          Act.findById(actId, function __actFindById(err, act) {
+            expect(err).to.not.exist;
+            expect(act).to.not.exist;
+
+            done();
+          });
+        });
+    }); // it()
+
+    describe('/likes', function __describe() {
+      var likeId = null;
+      var validLike = {
+        user: ObjectId()
+      };
+
+      beforeEach(function __beforeEach(done) {
+        validLike.target = { act: actId };
+
+        new Like(validLike).save(function __likeSave(err, like) {
+          if (err) { return done(err); }
+
+          likeId = like._id;
+
+          done();
+        });
+      }); // beforeEach()
+
+      afterEach(function __afterEach(done) {
+        Like.remove({}, function __likeRemove(err) {
+          if (err) { return done(err); }
+
+          done();
+        });
+      }); // afterEach()
+
+      it('GET should return status 200 and a non-empty array', function __it(done) {
+        request(app)
+          .get('/acts/' + actId + '/likes')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function __expect(res) {
+            expect(res.body).to.be.an('array').and.to.have.length.above(0);
+          })
+          .end(done);
+      }); // it()
+
+      it('POST invalid data should return status 400 and an error message', function __it(done) {
+        request(app)
+          .post('/acts/' + actId + '/likes')
+          .send({})
+          .expect(400)
+          .expect('Content-Type', /json/)
+          .expect(function __expect(res) {
+            expect(res.body).to.have.property('message', 'Like validation failed');
+          })
+          .end(done);
+      }); // it()
+
+      it('POST valid data should return status 201 and the created Like', function __it(done) {
+        request(app)
+          .post('/acts/' + actId + '/likes')
+          .send({ user: ObjectId() })
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .expect('Location', /likes/)
+          .expect(function __expect(res) {
+            expect(res.body).be.be.an('object').and.to.have.property('_id');
+            expect(res.body).to.have.deep.property('target.act', actId.toString());
+          })
+          .end(done);
+      }); // it()
+
+      describe('/:like_id', function __describe() {
+        it('DELETE invalid ID should return status 400 and an error message', function __it(done) {
+          request(app)
+            .delete('/acts/' + actId + '/likes/invalid')
+            .expect(400)
+            .expect('Content-Type', /json/)
+            .expect(function __expect(res) {
+              expect(res.body).to.have.property('message', 'Invalid like_id');
+            })
+            .end(done);
+        }); // it()
+
+        it('DELETE non-existent ID should return status 400 and an error message', function __it(done) {
+          request(app)
+            .delete('/acts/' + actId + '/likes/' + ObjectId())
+            .expect(400)
+            .expect('Content-Type', /json/)
+            .expect(function __expect(res) {
+              expect(res.body).to.have.property('message');
+            })
+            .end(done);
+        }); // it()
+
+        it('DELETE valid ID should return status 204 and delete the Like', function __it(done) {
+          request(app)
+            .delete('/acts/' + actId + '/likes/' + likeId)
+            .expect(204)
+            .expect(function __expect(res) {
+              expect(res.body).to.be.empty;
+            })
+            .end(function __end() {
+              Like.findById(likeId, function __likeFindById(err, like) {
+                expect(err).to.not.exist;
+                expect(like).to.not.exist;
+
+                done();
+              });
+            });
+        }); // it()
+      }); // describe()
+    }); // describe()
+
+    describe('/comments', function __describe() {
+      var commentId = null;
+      var validComment = {
+        user: ObjectId(),
+        content: { text: 'Comment text' }
+      };
+
+      beforeEach(function __beforeEach(done) {
+        validComment.target = { act: actId };
+
+        new Comment(validComment).save(function __commentSave(err, comment) {
+          if (err) { return done(err); }
+
+          commentId = comment._id;
+
+          done();
+        });
+      }); // beforeEach()
+
+      afterEach(function __afterEach(done) {
+        Comment.remove({}, function __commentRemove(err) {
+          if (err) { return done(err); }
+
+          done();
+        });
+      }); // afterEach()
+
+      it('GET should return status 200 and a non-empty array', function __it(done) {
+        request(app)
+          .get('/acts/' + actId + '/comments')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function __expect(res) {
+            expect(res.body).to.be.an('array').and.to.have.length.above(0);
+          })
+          .end(done);
+      }); // it()
+
+      it('POST invalid data should return status 400 and an error message', function __it(done) {
+        request(app)
+          .post('/acts/' + actId + '/comments')
+          .send({})
+          .expect(400)
+          .expect('Content-Type', /json/)
+          .expect(function __expect(res) {
+            expect(res.body).to.have.property('message', 'Comment validation failed');
+          })
+          .end(done);
+      }); // it()
+
+      it('POST valid data should return status 201 and the created Comment', function __it(done) {
+        request(app)
+          .post('/acts/' + actId + '/comments')
+          .send(validComment)
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .expect('Location', /comments/)
+          .expect(function __expect(res) {
+            expect(res.body).be.be.an('object').and.to.have.property('_id');
+            expect(res.body).to.have.deep.property('target.act', actId.toString());
+          })
+          .end(done);
+      }); // it()
+
+      describe('/:comment_id', function __describe() {
+        it('DELETE invalid ID should return status 400 and an error message', function __it(done) {
+          request(app)
+            .delete('/acts/' + actId + '/comments/invalid')
+            .expect(400)
+            .expect('Content-Type', /json/)
+            .expect(function __expect(res) {
+              expect(res.body).to.have.property('message', 'Invalid comment_id');
+            })
+            .end(done);
+        }); // it()
+
+        it('DELETE non-existent ID should return status 400 and an error message', function __it(done) {
+          request(app)
+            .delete('/acts/' + actId + '/comments/' + ObjectId())
+            .expect(400)
+            .expect('Content-Type', /json/)
+            .expect(function __expect(res) {
+              expect(res.body).to.have.property('message');
+            })
+            .end(done);
+        }); // it()
+
+        it('DELETE valid ID should return status 204 and delete the Comment', function __it(done) {
+          request(app)
+            .delete('/acts/' + actId + '/comments/' + commentId)
+            .expect(204)
+            .expect(function __expect(res) {
+              expect(res.body).to.be.empty;
+            })
+            .end(function __end() {
+              Comment.findById(commentId, function __commentFindById(err, comment) {
+                expect(err).to.not.exist;
+                expect(comment).to.not.exist;
+
+                done();
+              });
+            });
+        }); // it()
+
+        it('PUT extra data should be ignored', function __it(done) {
+          request(app)
+            .put('/acts/' + actId + '/comments/' + commentId)
+            .send({ extra: 'Extra data' })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function __expect(res) {
+              expect(res.body).to.have.property('_id', commentId.toString());
+              expect(res.body).to.not.have.property('extra');
+            })
+            .end(done);
+        }); // it()
+
+        it('PUT valid data should return status 200 and update the Comment', function __it(done) {
+          var update = { content: { text: 'Updated text' } };
+
+          request(app)
+            .put('/acts/' + actId + '/comments/' + commentId)
+            .send(update)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function __expect(res) {
+              expect(res.body).to.have.property('_id', commentId.toString());
+              expect(res.body).to.have.property('modified');
+              expect(res.body).to.have.deep.property('content.text', update.content.text);
+            })
+            .end(done);
+        }); // it()
+      }); // describe()
+    }); // describe()
+  }); // describe()
+}); // describe()
