e3b90aeddd0cfe113cf2f9dad4569156f688e586
Ben Booth <bkbooth@gmail.com>
2016-02-17 15:50:00 +1100

    Update and shrink-wrap npm dependencies
    Use node:4 as npm.sh base image for native build dependencies

--- npm.sh
+++ npm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Always pull latest image
-docker pull mhart/alpine-node:4
+docker pull node:4
 
 docker run \
     --rm -it \
     --name=npm-$(( $RANDOM % 99999 )) \
     -v $(pwd):/code \
     -w "/code" \
-    mhart/alpine-node:4 npm --quiet ${*}
+    node:4 npm --quiet ${*}
