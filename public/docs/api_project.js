dc108ea565aff6466989f985d74fa1abc0bcf4e0
Ben Booth <bkbooth@gmail.com>
2016-03-10 12:39:36 +1100

    v1.6.3

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -1,27 +1,1 @@
-define({
-  "name": "LIOW API Server",
-  "title": "LIOW API Server",
-  "url": "https://api.loveisourweapon.com",
-  "order": [
-    "Auth",
-    "Users",
-    "Deeds",
-    "Groups",
-    "Campaigns",
-    "Acts",
-    "Comments",
-    "Likes",
-    "Feeds",
-    "Countries"
-  ],
-  "version": "1.6.2",
-  "description": "The Love is our Weapon Campaign website API server",
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2016-03-10T01:09:25.846Z",
-    "url": "http://apidocjs.com",
-    "version": "0.15.1"
-  }
-});
+define({  "name": "LIOW API Server",  "title": "LIOW API Server",  "url": "https://api.loveisourweapon.com",  "order": [    "Auth",    "Users",    "Deeds",    "Groups",    "Campaigns",    "Acts",    "Comments",    "Likes",    "Feeds",    "Countries"  ],  "version": "1.6.3",  "description": "The Love is our Weapon Campaign website API server",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-03-10T01:39:35.889Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
